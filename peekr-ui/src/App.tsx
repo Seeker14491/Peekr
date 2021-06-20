e2eca2938af15462724dcc64363d91df065f44dd
Brian Bowman <seeker14491@gmail.com>
2021-06-19 22:09:32 -0500

    Refactor UI code

--- peekr-ui/src/App.tsx
+++ peekr-ui/src/App.tsx
@@ -1,33 +1,18 @@
-// TODO: Jump, Wing Open/Close, Reset
-// TODO: Fix rotations being inverted when flying
-
 import * as React from "react";
-import {
-  ChakraProvider,
-  Grid,
-  HStack,
-  Slider,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { ChakraProvider, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { theme } from "./theme";
-import { DigitalIndicator } from "./components/DigitalIndicator";
-import { TelemetryTable } from "./components/TelemetryTable";
-import { ControlStick } from "./components/ControlStick";
 import { getTelemetry, Telemetry } from "./telemetry/telemetry";
+import { DirectivesDisplay } from "./components/DirectivesDisplay";
 
 const animate = (setTelemetry: (x: Telemetry) => void) => {
   getTelemetry().then((response) => {
     if (response != null) {
       setTelemetry(response);
     }
-  });
 
-  requestAnimationFrame(() => animate(setTelemetry));
+    requestAnimationFrame(() => animate(setTelemetry));
+  });
 };
 
 export const App = () => {
@@ -40,72 +25,7 @@ export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       {telemetry ? (
-        <>
-          {/*<TelemetryTable telemetry={telemetry} />*/}
-          <HStack p={2}>
-            <ControlStick
-              x={telemetry.CarDirectives.steer}
-              y={telemetry.WingsOpen ? -telemetry.CarDirectives.rotationX : 0}
-            />
-            <VStack>
-              <Grid
-                templateColumns="repeat(2, 1fr)"
-                gap={1}
-                justifyItems="stretch"
-              >
-                <DigitalIndicator
-                  text="Boost"
-                  state={telemetry.CarDirectives.boost}
-                />
-                <DigitalIndicator
-                  text="Grip"
-                  state={telemetry.CarDirectives.grip}
-                />
-                <DigitalIndicator text="Wings" state={telemetry.WingsOpen} />
-                <DigitalIndicator
-                  text="Jump"
-                  state={telemetry.JumpTimer < 0.1}
-                />
-              </Grid>
-              <Slider
-                size="lg"
-                isReadOnly={true}
-                focusThumbOnChange={false}
-                max={1}
-                value={telemetry.CarDirectives.brake}
-              >
-                <SliderTrack>
-                  <SliderFilledTrack />
-                </SliderTrack>
-                <SliderThumb />
-              </Slider>
-            </VStack>
-            <Slider
-              orientation="vertical"
-              size="lg"
-              isReadOnly={true}
-              focusThumbOnChange={false}
-              min={-1}
-              max={1}
-              value={telemetry.CarDirectives.gas}
-              h="auto"
-              alignSelf="stretch"
-            >
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
-            <ControlStick
-              x={
-                telemetry.WingsOpen
-                  ? -telemetry.CarDirectives.rotationZ
-                  : telemetry.CarDirectives.rotationZ
-              }
-              y={telemetry.WingsOpen ? 0 : telemetry.CarDirectives.rotationX}
-            />
-          </HStack>
-        </>
+        <DirectivesDisplay telemetry={telemetry} />
       ) : (
         <Text>No telemetry</Text>
       )}
---
+++ peekr-ui/src/components/DirectivesDisplay.tsx
@@ -0,0 +1,66 @@
+import { Telemetry } from "../telemetry/telemetry";
+import { ControlStick } from "./ControlStick";
+import {
+  Grid,
+  HStack,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  VStack,
+} from "@chakra-ui/react";
+import { DigitalIndicator } from "./DigitalIndicator";
+import * as React from "react";
+
+export const DirectivesDisplay = ({ telemetry }: { telemetry: Telemetry }) => (
+  <HStack p={2}>
+    <ControlStick
+      x={telemetry.CarDirectives.steer}
+      y={telemetry.WingsOpen ? -telemetry.CarDirectives.rotationX : 0}
+    />
+    <VStack>
+      <Grid templateColumns="repeat(2, 1fr)" gap={1} justifyItems="stretch">
+        <DigitalIndicator text="Boost" state={telemetry.CarDirectives.boost} />
+        <DigitalIndicator text="Grip" state={telemetry.CarDirectives.grip} />
+        <DigitalIndicator text="Wings" state={telemetry.WingsOpen} />
+        <DigitalIndicator text="Jump" state={telemetry.JumpTimer < 0.1} />
+      </Grid>
+      <Slider
+        size="lg"
+        isReadOnly={true}
+        focusThumbOnChange={false}
+        max={1}
+        value={telemetry.CarDirectives.brake}
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+    </VStack>
+    <Slider
+      orientation="vertical"
+      size="lg"
+      isReadOnly={true}
+      focusThumbOnChange={false}
+      min={-1}
+      max={1}
+      value={telemetry.CarDirectives.gas}
+      h="auto"
+      alignSelf="stretch"
+    >
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      <SliderThumb />
+    </Slider>
+    <ControlStick
+      x={
+        telemetry.WingsOpen
+          ? -telemetry.CarDirectives.rotationZ
+          : telemetry.CarDirectives.rotationZ
+      }
+      y={telemetry.WingsOpen ? 0 : telemetry.CarDirectives.rotationX}
+    />
+  </HStack>
+);
