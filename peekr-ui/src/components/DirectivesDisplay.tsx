8b5cee5c99c722b77b80593c15ab6e39cd8aaf22
Brian Bowman <seeker14491@gmail.com>
2021-06-20 22:41:21 -0500

    Add labels to Gas and Break sliders
    Also, remove the round handles from the sliders.

--- peekr-ui/src/components/DirectivesDisplay.tsx
+++ peekr-ui/src/components/DirectivesDisplay.tsx
@@ -5,55 +5,57 @@ import {
   HStack,
   Slider,
   SliderFilledTrack,
-  SliderThumb,
   SliderTrack,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 import { DigitalIndicator } from "./DigitalIndicator";
 import * as React from "react";
 
 export const DirectivesDisplay = ({ telemetry }: { telemetry: Telemetry }) => (
-  <HStack p={4}>
+  <HStack p={4} alignItems="stretch">
     <ControlStick
       x={telemetry.CarDirectives.steer}
       y={telemetry.WingsOpen ? -telemetry.CarDirectives.rotationX : 0}
     />
-    <VStack>
+    <VStack alignItems="stretch">
       <Grid templateColumns="repeat(2, 1fr)" gap={1} justifyItems="stretch">
         <DigitalIndicator text="Boost" state={telemetry.CarDirectives.boost} />
         <DigitalIndicator text="Grip" state={telemetry.CarDirectives.grip} />
         <DigitalIndicator text="Wings" state={telemetry.WingsOpen} />
         <DigitalIndicator text="Jump" state={telemetry.JumpTimer < 0.1} />
       </Grid>
+      <HStack>
+        <Text fontSize="sm">Brake:</Text>
+        <Slider
+          size="lg"
+          isReadOnly={true}
+          focusThumbOnChange={false}
+          max={1}
+          value={telemetry.CarDirectives.brake}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+        </Slider>
+      </HStack>
+    </VStack>
+    <VStack>
+      <Text fontSize="sm">Gas:</Text>
       <Slider
+        orientation="vertical"
         size="lg"
         isReadOnly={true}
         focusThumbOnChange={false}
+        min={-1}
         max={1}
-        value={telemetry.CarDirectives.brake}
+        value={telemetry.CarDirectives.gas}
       >
         <SliderTrack>
           <SliderFilledTrack />
         </SliderTrack>
-        <SliderThumb />
       </Slider>
     </VStack>
-    <Slider
-      orientation="vertical"
-      size="lg"
-      isReadOnly={true}
-      focusThumbOnChange={false}
-      min={-1}
-      max={1}
-      value={telemetry.CarDirectives.gas}
-      h="auto"
-      alignSelf="stretch"
-    >
-      <SliderTrack>
-        <SliderFilledTrack />
-      </SliderTrack>
-      <SliderThumb />
-    </Slider>
     <ControlStick
       x={
         telemetry.WingsOpen
