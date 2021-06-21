9acfc6aad07fb94a2233ebd1acbc34b8d7ea6bd2
Brian Bowman <seeker14491@gmail.com>
2021-06-20 22:08:03 -0500

    Add a build script for making a tiny executable on Windows

---
+++ peekr-ui/src-tauri/windows_tiny_build.bat
@@ -0,0 +1,2 @@
+cargo +nightly-x86_64-pc-windows-msvc build --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-pc-windows-msvc
+upx --ultra-brute .\target\x86_64-pc-windows-msvc\release\peekr.exe
